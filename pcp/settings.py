732b7423e6121ebab4b36c2f02b54c415d7fce4c
eneasteles <eneasteles@gmail.com>
2021-06-14 14:39:05 -0300

    third

--- pcp/settings.py
+++ pcp/settings.py
@@ -74,11 +74,22 @@ WSGI_APPLICATION = 'pcp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'de0luuhfnfaul4',
+
+        'USER': 'ezwdfevynjhsqj',
+
+        'PASSWORD': 'juparana',
+
+        'HOST': '88fd242d156c90feeb03b47752aba662be11e182cfeb5db30b032bf166adaa33',
+
+        'PORT': '5432',
+
     }
-}
 
 
 # Password validation
