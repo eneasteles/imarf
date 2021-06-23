e8d062d8d735fbf7419cd742d89452964a67f5d4
eneasteles <eneasteles@gmail.com>
2021-06-23 10:29:35 -0300

    pagina inicial2

--- producao/urls.py
+++ producao/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('current_datetime/', views.current_datetime, name='current_datetime')
-   
+    path('current_datetime/', views.current_datetime, name='current_datetime'),
+    path('', views.views.home, name='home')
 
 ]
