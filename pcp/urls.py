9dc5e9354080ebc0255ae2d0e0a53d23c653acbc
eneasteles <eneasteles@gmail.com>
2021-06-23 10:37:46 -0300

    pagina inicial5

--- pcp/urls.py
+++ pcp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("producao.urls")),
+    path('producao/', include("producao.urls")),
 ]
