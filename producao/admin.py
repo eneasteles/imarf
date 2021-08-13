acfbb85ad44cbf36137469b36838caf535500db2
eneasteles <eneasteles@gmail.com>
2021-08-13 14:21:17 -0300

    title

--- producao/admin.py
+++ producao/admin.py
@@ -3,6 +3,10 @@ from django.contrib.admin.filters import ListFilter
 
 from . models import *
 
+admin.site.site_header = 'SISTEMA DE CONTROLE DE PRODUÇÃO - IMARF'
+admin.site.site_title = 'PRODUÇÃO'
+admin.site.index_title = 'PCP IMARF'
+
 #Acabamento, Bloco, Chapa, Fio_diamantado, Insumo, Maquina, Material, Observacao_chapa, Pessoas, Serrada, Status_bloco, Status_chapa, Chapas_produzidas, Espessura, Unidade
 class Chapas_produzidasinline(admin.TabularInline):
     model = Chapas_produzidas
