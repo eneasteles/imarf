be206ae9919404afce466ece9aea0b6d453b2767
eneasteles <eneasteles@gmail.com>
2021-09-28 12:20:41 -0300

    setor pessoal search

--- setor_pessoal/admin.py
+++ setor_pessoal/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 @admin.register(Cadastro_Funcionario)
 class Cadastro_FuncionarioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'Centro_de_Custo', 'folha')
-    search_fields = ('nome', 'cpf', 'Centro_de_Custo')
+    list_display = ('nome', 'cpf',  'folha','outros','Centro_de_Custo',)
+    search_fields = ('nome', 'cpf', )
     list_filter = ( 'cpf', 'Centro_de_Custo')
-    list_editable = ('cpf','Centro_de_Custo','folha',)
+    list_editable = ('cpf','folha','outros','Centro_de_Custo',)
 
