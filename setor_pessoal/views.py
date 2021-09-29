02312d5f9b13eeb4d8bf45dd05cb7f4b5fea2899
eneasteles <eneasteles@gmail.com>
2021-09-29 16:17:13 -0300

    centro de custo 2

--- setor_pessoal/views.py
+++ setor_pessoal/views.py
@@ -2,7 +2,7 @@ from django.db.models import Sum, Max, Min, Avg
 from django.db.models.aggregates import Avg, Min
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
-from producao.models import *
+from .models import *
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 class Resumo(LoginRequiredMixin ,ListView):
     login_url = reverse_lazy('login')
-    model = 'Resumo_Funcionarios_View'
+    model = Resumo_Funcionarios_View
     template_name = 'setor_pessoal/resumo.html'
 
     
