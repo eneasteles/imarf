1523e7ed7f6890897c20d8db3f22da9576de9307
Eneas  Teles <eneasteles@gmail.com>
2021-11-01 13:51:35 -0300

    modelo seguro

--- caixa/admin.py
+++ caixa/admin.py
@@ -36,11 +36,7 @@ class CaixaAdmin(admin.ModelAdmin):
 
 @admin.register(Filial)
 class FilialAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(user_id=request.user)
+    
     def save_model(self, request, obj, form, change):
         if not change:
             obj.user = request.user
